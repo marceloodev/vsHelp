4f26e6d5f3f3ccda7289d116adcc1120d404470e
Vieira's Store <98975919+VieirasStore@users.noreply.github.com>
2022-08-20 15:21:07 -0300

    Added files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 ------------------------------------------------------------------------------
 -- vsHelp | By: Vieira's Store
 ------------------------------------------------------------------------------
-fx_version 'bodacious' -- NÃO MEXA <3
-game 'gta5' -- NÃO MEXA <3
+fx_version 'bodacious'
+game 'gta5'
 
-developer "Vieira's Store" -- NÃO MEXA <3
-version "1.0" -- NÃO MEXA <3
+developer "Vieira's Store"
+version "1.0"
 
-client_script { -- NÃO MEXA <3
-    "vs-config.lua", -- NÃO MEXA <3
-    "vs-client.lua"  -- NÃO MEXA <3
-} -- NÃO MEXA <3
+client_script {
+    "vs-config.lua",
+    "vs-client.lua" 
+}
--- vs-client.lua
+++ vs-client.lua
@@ -1,23 +1,23 @@
 ------------------------------------------------------------------------------
 -- vsHelp | By: Vieira's Store
 ------------------------------------------------------------------------------
-RegisterCommand(""..cfg.helpCommand.."", function() -- NÃO MEXA <3
-    msg(""cfg.FirstMessage"") -- NÃO MEXA <3
-    msg(""cfg.SecondMessage"") -- NÃO MEXA <3
-    msg(""cfg.ThirdMessage"") -- NÃO MEXA <3
-    msg(""cfg.FourthMessage"") -- NÃO MEXA <3
-    msg(""cfg.FifthMessage"") -- NÃO MEXA <3
-end, false) -- NÃO MEXA <3
+RegisterCommand(""..cfg.helpCommand.."", function()
+    msg(""cfg.FirstMessage"")
+    msg(""cfg.SecondMessage"")
+    msg(""cfg.ThirdMessage"")
+    msg(""cfg.FourthMessage"")
+    msg(""cfg.FifthMessage"")
+end, false)
 
-function msg(text) -- NÃO MEXA <3
-    TriggerEvent("chatMessage", ""..cfg.Name.."", {""..cfg.Color..""}, text) -- NÃO MEXA <3
-end -- NÃO MEXA <3
+function msg(text)
+    TriggerEvent("chatMessage", ""..cfg.Name.."", {""..cfg.Color..""}, text)
+end
 
 ------------------------------------------------------------------------------
 -- AGRADECIMENTO/AVISOS
 ------------------------------------------------------------------------------
-local VSDevelopment = "Obrigado por baixar nosso script!" -- NÃO MEXA <3
+local VSDevelopment = "Obrigado por baixar nosso script!"
 
-if VSDevelopment == "Obrigado por baixar nosso script!" then -- NÃO MEXA <3
-	print("^2[vsHelp] Obrigado por utilizar nosso script! Futuros updates em -> https://discord.gg/gJyMqQb8Nh!^0") -- NÃO MEXA <3
-end -- NÃO MEXA <3
+if VSDevelopment == "Obrigado por baixar nosso script!" then
+	print("^2[vsHelp] Obrigado por utilizar nosso script! Futuros updates em -> https://discord.gg/gJyMqQb8Nh!^0")
+end
