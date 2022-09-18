b29f9ce2c85217a4f10308f427b32398f9a5cc19
Vieira's Store <98975919+VieirasStore@users.noreply.github.com>
2022-09-18 13:06:27 -0300

    Deleted vs-server.lua, it was unnecessary.

--- vs-server.lua
+++
@@ -1,8 +0,0 @@
-------------------------------------------------------------------------------
--- AGRADECIMENTO/AVISOS
-------------------------------------------------------------------------------
-local VSDevelopment = "Obrigado por baixar nosso script!"
-
-if VSDevelopment == "Obrigado por baixar nosso script!" then
-	print("^2[vsHelp] Obrigado por utilizar nosso script! Futuros updates em -> https://discord.gg/gJyMqQb8Nh!^0")
-end
